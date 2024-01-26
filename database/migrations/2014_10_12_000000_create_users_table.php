d0317433972c991f8a8174c3da31781c8b90af27
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-25 23:31:33 -0600

    Add role relationship to User model and update users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->dateTime('last_login')->nullable();
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
